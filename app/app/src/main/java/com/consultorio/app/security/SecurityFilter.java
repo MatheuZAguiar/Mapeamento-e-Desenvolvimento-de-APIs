f78d973563f0ceeca992dbd5951a252658738141
MatheuZAguiar <matheuspaguiar099@gmail.com>
2024-05-16 01:48:55 -0300

    Bearer Token Works git add .!

--- app/app/src/main/java/com/consultorio/app/security/SecurityFilter.java
+++ app/app/src/main/java/com/consultorio/app/security/SecurityFilter.java
@@ -15,9 +15,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
-
 public class SecurityFilter extends OncePerRequestFilter {
-
     @Autowired
     TokenService tokenService;
     @Autowired
@@ -26,19 +24,19 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
-        if (token != null) {
+        if(token != null){
             var login = tokenService.validateToken(token);
             UserDetails user = userRepository.findByLogin(login);
 
-            var authentication = new UsernamePasswordAuthenticationToken(user , null , user.getAuthorities());
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
-        filterChain.doFilter(request,response);
+        filterChain.doFilter(request, response);
     }
 
     private String recoverToken(HttpServletRequest request){
-       var authHeader = request.getHeader("Authorization");
-       if (authHeader == null) return null;
-       return authHeader.replace("Bearer","");
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
     }
 }
